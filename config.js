1c21592f79ef2bd6974246cacbd740ad106f0f72
dineth panditha <92966321+dinethh@users.noreply.github.com>
2025-06-13 16:11:53 +0530

    aad env in to confile file

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 
 
-dotenv.config({ path: "../lms-backend/.env" });
+dotenv.config({ path: "../backend/.env" });
 
 
 export const config = {
@@ -31,3 +31,30 @@ export const config = {
   if (!config.JWT_SECRET) {
     throw new Error("JWT_SECRET is missing");
   }
+
+
+
+
+      // project root eke  .env file ekak hadala meka daganna
+
+// # Server
+// PORT=5001
+// MONGO_URI=mongodb://localhost:27017/lms-app
+// JWT_SECRET=1a4b8f3fd3d91a7ec26be25903879b0153797cc951ea8f789fef311e65e47f3b
+
+// # AWS S3 Configuration
+// AWS_REGION=ap-south-1
+// AWS_ACCESS_KEY_ID=AKIAVY2PG4ZRKZ4BN7GO
+// AWS_SECRET_ACCESS_KEY=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
+// AWS_BUCKET_NAME=lms-app1
+
+// # Email Configuration
+// EMAIL_USER=d1611841@gmail.com
+// EMAIL_PASS=cvvg lcgl wijx frie
+// EMAIL_HOST=smtp.gmail.com
+// EMAIL_PORT=587
+
+// # OpenAI GPT
+// GPT_TOKEN=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
+// GPT_ENDPOINT=https://models.inference.ai.azure.com
+// GPT_MODEL=gpt-4o
